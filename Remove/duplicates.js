8e891732b6773112d7600ce8f94bc73da652a5ab
thomassmith0526 <thomassmith0526@gmail.com>
2024-07-14 13:27:51 -0500

    duplicates

---
+++ Remove/duplicates.js
@@ -0,0 +1,16 @@
+function removeDuplicates(arr) {
+    let i = 0;
+    for (let j = 1; j< arr.length; j++) {
+           if(arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+     return i + 1;
+}
+const arr = [1,1,2,2,2,3,3];
+const k =removeDuplicates(arr);
+console.log('The array after removing duplicates elements is:');
+for (let i = 0; i < k; i++) {
+    console.log(arr[i])
+}
---
+++ Remove/duplicates.md
@@ -0,0 +1,4 @@
+set i to 0
+write a for loop and let j = 1
+if(arr[i] !== arr[j])
+!== means not equal too
